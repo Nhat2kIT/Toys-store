407b9cc6733b07a8c3de30efea839c031b4c2bbd
Nhat2kIT <130134277+Nhat2kIT@users.noreply.github.com>
2025-03-28 10:25:17 +0700

    Add files via upload

---
+++ my_project/Views/introduce/about.php
@@ -0,0 +1,109 @@
+
+<!-- pages-title-start -->
+<div class="pages-title section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="pages-title-text text-center">
+					<h2>Giới Thiệu</h2>
+					<ul class="text-left">
+						<li><a href="?act=home">Trang chủ</a></li>
+						<li><span> // </span>Giới Thiệu</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- about-us-section-start -->
+<div class="pages about-us section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="main-padding section-padding-top clearfix">
+				<div class="col-sm-12 col-md-5">
+					<div class="about-img">
+						<img src="\my_project\public\img\about\1.jpg" alt="" />
+					</div>
+					</div>
+                        <div class="col-sm-12 col-md-6">
+                          <div class="about-text">
+                          <div class="about-author">
+                              <h2 style="font-size: 24px;">Cửa hàng đồ chơi Thiên Long</h2>
+                             </div>
+                           <h3 style="font-size: 18px;">Cửa hàng chúng tôi với:</h3>
+                             <ol style="font-size: 18px;">
+                                   <li>Chất lượng vượt trội: Đồ chơi an toàn, bền bỉ, đáp ứng mọi tiêu chuẩn cao nhất trên thị trường.</li>
+                                   <li>Dịch vụ tận tâm: Đội ngũ chuyên nghiệp, thân thiện, luôn sẵn sàng hỗ trợ khách hàng.</li>
+                                   <li>Nguồn hàng uy tín: Đối tác đáng tin cậy, đảm bảo chất lượng và nguồn cung ổn định.</li>
+                                   <li>Ưu đãi hấp dẫn: Chính sách khuyến mãi và chăm sóc tận tình, mang lại giá trị tốt nhất.</li>
+                                   <li>Phát triển sáng tạo: Đồ chơi không chỉ bền mà còn kích thích trí tuệ và sự sáng tạo của trẻ.</li>
+                                </ol>
+                               <p style="font-size: 18	px;">Với những chế độ đó chúng tôi hứa sẽ mang lại cho khách một sự hài lòng nhất khi đến với cửa hàng của chúng tôi !!!</p>
+                           </div>
+                        </div>
+                       </div>
+		            </div>
+	           </div>
+             </div>
+<!-- about-us section end -->
+<!-- team-member section start -->
+<section class="pages team-member section-padding-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="section-title text-center">
+					<h3>Team Tạo Trang Web</h3>
+				</div>
+			</div>
+		</div>
+		<div class="row text-center">
+			
+			<!-- single member end -->
+			<div class="row">
+    <!-- Thành viên 1 -->
+    <div class="col-sm-12 col-md-4">
+        <div class="single-member">
+            <div class="member-img">
+                <a href="#"><img src="\my_project\public\img\about\NhY.jpg" alt="Team Member" /></a>
+            </div>
+            <div class="share-member">
+                <div class="member-title">
+                    <h4>Phạm Huỳnh Nhật Ý</h4>
+                </div>
+                <div class="member-btn">    
+                    <a href="https://www.facebook.com/Nhat.Y.2000"><i class="mdi mdi-facebook"></i></a>
+                    <a href="#"><i class="mdi mdi-twitter"></i></a>
+                    <a href="#"><i class="mdi mdi-google-plus"></i></a>
+                    <a href="#"><i class="mdi mdi-dribbble"></i></a>
+                    <a href="#"><i class="mdi mdi-rss"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Thành viên 2 -->
+    <div class="col-sm-12 col-md-4">
+        <div class="single-member">
+            <div class="member-img">
+                <a href="#"><img src="\my_project\public\img\about\HTri.jpg" alt="Team Member" /></a>
+            </div>
+            <div class="share-member">
+                <div class="member-title">
+                    <h4>Đỗ Hữu Trí</h4>
+                </div>
+                <div class="member-btn">    
+                    <a href="https://www.facebook.com/profile.php?id=100029966986178"><i class="mdi mdi-facebook"></i></a>
+                    <a href="#"><i class="mdi mdi-twitter"></i></a>
+                    <a href="#"><i class="mdi mdi-google-plus"></i></a>
+                    <a href="#"><i class="mdi mdi-dribbble"></i></a>
+                    <a href="#"><i class="mdi mdi-rss"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+			
+
+<!-- team-member section end -->
